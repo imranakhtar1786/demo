6db3b889f19e72310b4de30c876cdbf2fc01f70d
imranakhtar1786 <imranakhtar1786@gmail.com>
2025-11-30 11:22:49 +0530

    2

--- indexd.js
+++ indexd.js
@@ -19,6 +19,37 @@ const swiper = new Swiper(".mySwiper", {
 });
 
 
+// document.addEventListener("DOMContentLoaded", () => {
+//   const swiper = new Swiper(".cardSwiper", {
+//     loop: true,
+//     speed: 700,
+
+//     autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: false,
+//     },
+
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+
+//     breakpoints: {
+//       1440: { slidesPerView: 5, spaceBetween: 20 },
+//       1200: { slidesPerView: 4, spaceBetween: 20 },
+//       992: { slidesPerView: 3, spaceBetween: 20 },
+//       768: { slidesPerView: 2, spaceBetween: 20 },
+//       0: { slidesPerView: 1, spaceBetween: 10 }
+//     }
+//   });
+// });
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const swiper = new Swiper(".cardSwiper", {
     loop: true,
@@ -28,6 +59,9 @@ document.addEventListener("DOMContentLoaded", () => {
       delay: 2500,
       disableOnInteraction: false,
     },
+
+    // centeredSlides: true,               // ✅ Center slides
+    centerInsufficientSlides: true,     // ✅ Center even if slides < slidesPerView
 
     pagination: {
       el: ".swiper-pagination",
@@ -40,7 +74,8 @@ document.addEventListener("DOMContentLoaded", () => {
     },
 
     breakpoints: {
-      1200: { slidesPerView: 5, spaceBetween: 20 },
+      1440: { slidesPerView: 5, spaceBetween: 20 },
+      1200: { slidesPerView: 3.5, spaceBetween: 20 },
       992: { slidesPerView: 3, spaceBetween: 20 },
       768: { slidesPerView: 2, spaceBetween: 20 },
       0: { slidesPerView: 1, spaceBetween: 10 }
